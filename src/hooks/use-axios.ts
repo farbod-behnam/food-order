9393bad4d39c94d5526f575883804e365f313211
farbod-behnam <farbod.behnam@gmail.com>
2023-04-17 10:40:39 +0330

    create custom hook for axios

---
+++ src/hooks/use-axios.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+
+export default function useAxios<T>() {
+    const [data, setData] = useState<T | undefined>(undefined);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [controller, setController] = useState<AbortController>();
+
+    const axiosSendRequest = async (config: AxiosRequestConfig<T>) => {
+
+        const ctrl = new AbortController();
+        setController(ctrl);
+
+        try {
+            setIsLoading(true);
+
+            const response = await axios.request({...config, signal: ctrl.signal});
+            setData(response.data);
+
+        } catch (err) {
+            console.log(err);
+            if (axios.isAxiosError(err)) {
+                setError(err.message);
+            }
+            else {
+                const error = err as Error;
+                setError(error.message || "Something went wrong!");
+            }
+            
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+
+        return () => {
+            controller && controller.abort();
+        }
+
+    }, [controller]);
+
+    return [data, error, isLoading, axiosSendRequest] as const;
+}
